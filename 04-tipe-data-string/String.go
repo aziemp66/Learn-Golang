0a6f985ef6dab0e78e0bb7d796a6273460dd5381
Azie Melza Pratama <aziemp66@gmail.com>
2022-10-06 11:06:05 +0700

    Tipe Data String

---
+++ 04-tipe-data-string/String.go
@@ -0,0 +1,9 @@
+package main
+
+import "fmt"
+
+func main() {
+	fmt.Println("Azie Melza Pratama")
+	fmt.Println(len("Azie Melza Pratama"))
+	fmt.Println("Azie Melza Pratama"[1]) //Convert to ASCII
+}
