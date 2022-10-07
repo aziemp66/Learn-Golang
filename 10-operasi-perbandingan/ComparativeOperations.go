6fcbc636f59b53a41f84db0de54b937b1e1a3127
Azie Melza Pratama <aziemp66@gmail.com>
2022-10-07 17:54:01 +0700

    Operasi Perbandingan

---
+++ 10-operasi-perbandingan/ComparativeOperations.go
@@ -0,0 +1,25 @@
+package main
+
+import "fmt"
+
+func main() {
+	// number1 := 10
+	// number2 := 20
+
+	// fmt.Println(number1 == number2)
+	// fmt.Println(number1 != number2)
+	// fmt.Println(number1 < number2)
+	// fmt.Println(number1 > number2)
+	// fmt.Println(number1 <= number2)
+	// fmt.Println(number1 >= number2)
+
+	firstName := "John"
+	lastName := "Doe"
+
+	firstName2 := "John"
+	lastName2 := "Kepler"
+
+	fmt.Println("My name is", firstName, lastName)
+	fmt.Println("His name is", firstName2, lastName2)
+	fmt.Println(firstName == firstName2)
+}
