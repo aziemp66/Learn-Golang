eabc3d0cc0b15fe625076d4b3e869d6c335154d4
Azie Melza Pratama <aziemp66@gmail.com>
2022-10-17 08:25:19 +0700

    Math

---
+++ 48-package-math/Math.go
@@ -0,0 +1,14 @@
+package main
+
+import (
+	"fmt"
+	"math"
+)
+
+func main() {
+	fmt.Println(math.Ceil(1.34))
+	fmt.Println(math.Floor(1.9))
+	fmt.Println(math.Round(1.50))
+	fmt.Println(math.Max(3, 7))
+	fmt.Println(math.Min(8, 2))
+}
